e86e0ba020ab81330d4116ea5ce59979f3002f1b
Meet Thakkar <meet.h.thakkar@gmail.com>
2017-04-10 23:38:20 -0400

    adding a logging for debugging so println works

--- Tools/autotest/run_in_terminal_window.sh
+++ Tools/autotest/run_in_terminal_window.sh
@@ -8,17 +8,23 @@ shift
 echo "RiTW: Starting $name : $*"
 # default to xterm as it has the most consistent options and can start minimised
 if [ -n "$DISPLAY" -a -n "$(which osascript)" ]; then
+  echo "A"
   osascript -e 'tell application "Terminal" to do script "'"$* "'"'
 elif [ -n "$DISPLAY" -a -n "$(which xterm)" ]; then
-  xterm -iconic -xrm 'XTerm*selectToClipboard: true' -xrm 'XTerm*initialFont: 6' -n "$name" -name "$name" -T "$name" -hold -e $* &
+  echo "B"
+  xterm -iconic -xrm 'XTerm*selectToClipboard: true' -xrm 'XTerm*initialFont: 6' -n "$name" -name "$name" -T "$name" -hold -e "$* 2>&1> /tmp/hex.log" &
 elif [ -n "$DISPLAY" -a -n "$(which konsole)" ]; then
+  echo "c"
   konsole --hold -e $*
 elif [ -n "$DISPLAY" -a -n "$(which gnome-terminal)" ]; then
+  echo "D"
   gnome-terminal -e "$*"
 elif [ -n "$STY" ]; then
+  echo "E"
   # We are running inside of screen, try to start it there
   screen -X screen -t "$name" $*
 else
+  echo "F"
   filename="/tmp/$name.log"
   echo "RiTW: Window access not found, logging to $filename"
   cmd="$1"
